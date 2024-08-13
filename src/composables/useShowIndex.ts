caa070b6e4bdb057a8bddd51840a57748077705e
Serge Arbor <serge.arbor@gmail.com>
2024-08-13 21:15:04 +0200

    sort shows by rating

--- src/composables/useShowIndex.ts
+++ src/composables/useShowIndex.ts
@@ -1,43 +1,29 @@
 import { useQuery } from '@tanstack/vue-query'
-import { createSharedComposable } from '@vueuse/core'
 import { computed, type Ref } from 'vue'
 
-import { type Genre, type Show } from '@/entities/show.entity'
+import { type GenreId, type Show } from '@/entities/show.entity'
 import { fetchShows } from '@/services/showService'
 
-export function useShowIndex(activeGenres: Ref<Set<Genre>>) {
+export function useShowIndex(activeGenres: Ref<Set<GenreId>>) {
   const queryInstance = useQuery({
     queryKey: ['show-index'],
     queryFn: fetchShows,
     networkMode: 'offlineFirst'
   })
 
-  const genres = computed<Set<Genre>>(() => {
-    const shows = queryInstance.data.value ?? []
-    const genres = new Set<Genre>()
-
-    shows.forEach((show) => {
-      show.genres.forEach((genre) => genres.add(genre))
-    })
-
-    return genres
-  })
-
   const filteredShows = computed<Show[] | undefined>(() => {
-    if (activeGenres.value.size === 0) {
-      return queryInstance.data.value
-    }
+    const shows = queryInstance.data.value || []
 
-    return queryInstance.data.value?.filter((show) =>
-      show.genres.some((genre) => activeGenres.value?.has(genre))
-    )
+    const filtered =
+      activeGenres.value.size > 0
+        ? shows.filter((show) => show.genres.some((genre) => activeGenres.value.has(genre)))
+        : shows
+
+    return [...filtered].sort((a, b) => (b.rating?.average ?? 0) - (a.rating?.average ?? 0))
   })
 
   return {
     ...queryInstance,
-    genres,
     filteredShows
   }
 }
-
-export const useSharedShowIndex = createSharedComposable(useShowIndex)
