5b75f64710a34a576a40ff973dfd2588d62910bc
Serge Arbor <serge.arbor@gmail.com>
2024-08-14 18:19:44 +0200

    sync state with the route

---
+++ src/composables/useShowListFilters.ts
@@ -0,0 +1,38 @@
+import { refWithControl } from '@vueuse/core'
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+import type { GenreId } from '@/entities/show.entity'
+
+export function useShowListFilters() {
+  const route = useRoute()
+  const router = useRouter()
+
+  const initialGenresValue = route.query.genres ? (route.query.genres as string).split(',') : []
+  const selectedGenres = ref<Set<GenreId>>(new Set(initialGenresValue))
+
+  const initialSearchQuery = route.query.q ?? ''
+  const searchQuery = refWithControl(initialSearchQuery, {
+    onChanged: () => {
+      selectedGenres.value.clear()
+    }
+  })
+
+  watch(
+    [searchQuery, selectedGenres],
+    ([newQuery, newGenres]) => {
+      router.replace({
+        query: {
+          q: newQuery || undefined,
+          genres: newGenres.size ? Array.from(newGenres).join(',') : undefined
+        }
+      })
+    },
+    { deep: true }
+  )
+
+  return {
+    selectedGenres,
+    searchQuery
+  }
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,20 @@
 <script setup lang="ts">
-import { refWithControl } from '@vueuse/core'
-import { ref } from 'vue'
-
 import GenresFilter from '@/components/GenresFilter.vue'
 import SearchBar from '@/components/SearchBar.vue'
 import ShowList from '@/components/ShowList.vue'
 import { useFilteredShows } from '@/composables/useFilteredShows'
 import { useGenres } from '@/composables/useGenres'
 import { useShowList } from '@/composables/useShowList'
-import type { GenreId } from '@/entities/show.entity'
+import { useShowListFilters } from '@/composables/useShowListFilters'
 
-const selectedGenres = ref<Set<GenreId>>(new Set())
-const searchQuery = refWithControl('', {
-  onChanged: () => {
-    selectedGenres.value.clear()
-  }
-})
+const { selectedGenres, searchQuery } = useShowListFilters()
 const { data: showList, error, isLoading } = useShowList(searchQuery)
 const { data: filteredShowList } = useFilteredShows(showList, selectedGenres)
 const { genres } = useGenres(showList)
 </script>
 
 <template>
-  <main class="p-4 flex flex-col gap-4">
+  <main class="p-4 flex flex-col gap-4 max-w-screen-lg mx-auto">
     <SearchBar v-model="searchQuery" />
 
     <GenresFilter v-model="selectedGenres" :genres="genres" :is-loading="isLoading" />
