f2f6d2f6256a166cb7b347247beed21071d079df
Serge Arbor <serge.arbor@gmail.com>
2024-08-14 16:58:16 +0200

    Implement Skeleton for the ShowDetails

--- src/components/ShowDetails.vue
+++ src/components/ShowDetails.vue
@@ -1,24 +1,54 @@
 <script setup lang="ts">
 import DOMPurify from 'dompurify'
+import { NSkeleton } from 'naive-ui'
 import { computed } from 'vue'
 
-import { useFetchShowDetails } from '@/composables/useFetchShowDetails'
+import type { Show } from '@/entities/show.entity'
 
 const props = defineProps<{
-  id: string
+  show: Show | undefined
+  error: Error | null
+  isLoading: boolean
 }>()
 
 const sanitizedSummary = computed(() => {
-  return show.value?.summary ? DOMPurify.sanitize(show.value.summary) : ''
+  return props.show?.summary ? DOMPurify.sanitize(props.show.summary) : ''
 })
-
-const { data: show, error, isLoading } = useFetchShowDetails(props.id)
 </script>
 
 <template>
   <div class="p-4 max-w-4xl mx-auto">
-    <div v-if="isLoading" class="text-center text-gray-500">Loading show details...</div>
+    <div v-if="isLoading" class="space-y-6">
+      <NSkeleton text :repeat="1" height="48px" width="50%" />
+
+      <div class="flex flex-col md:flex-row gap-6">
+        <div class="relative w-full md:w-1/3" style="aspect-ratio: 1250 / 1800">
+          <NSkeleton :text="false" class="absolute top-0 left-0 size-full rounded-lg" />
+        </div>
+
+        <div class="space-y-2 flex-1">
+          <div>
+            <NSkeleton text :repeat="1" height="20px" width="30%" />
+          </div>
+          <div><NSkeleton text :repeat="1" height="20px" width="30%" /></div>
+          <div><NSkeleton text :repeat="1" height="20px" width="30%" /></div>
+          <div><NSkeleton text :repeat="1" height="20px" width="50%" /></div>
+          <div><NSkeleton text :repeat="1" height="20px" width="30%" /></div>
+          <div><NSkeleton text :repeat="1" height="20px" width="40%" /></div>
+          <div><NSkeleton text :repeat="1" height="20px" width="20%" /></div>
+          <div><NSkeleton text :repeat="1" height="20px" width="20%" /></div>
+          <div><NSkeleton text :repeat="1" height="20px" width="30%" /></div>
+          <div><NSkeleton text :repeat="1" height="20px" width="50%" /></div>
+        </div>
+      </div>
+
+      <div>
+        <NSkeleton text :repeat="6" height="20px" width="100%" class="mb-2" />
+      </div>
+    </div>
+
     <div v-if="error" class="text-center text-red-500">Error: {{ error.message }}</div>
+
     <div v-if="show" class="space-y-6">
       <h1 class="text-3xl font-bold text-gray-800">{{ show.name }}</h1>
       <div class="flex flex-col md:flex-row gap-6">
--- src/views/ShowDetailsView.vue
+++ src/views/ShowDetailsView.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import ShowDetails from '@/components/ShowDetails.vue'
-defineProps<{
+import { useFetchShowDetails } from '@/composables/useFetchShowDetails'
+
+const props = defineProps<{
   id: string
 }>()
+
+const { data: show, error, isLoading } = useFetchShowDetails(props.id)
 </script>
 
 <template>
-  <ShowDetails :id="id" />
+  <ShowDetails :show="show" :error="error" :is-loading="isLoading" />
 </template>
