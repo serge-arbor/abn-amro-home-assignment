e107e37284493513db286bf371c4186fb3c5a34c
Serge Arbor <serge.arbor@gmail.com>
2024-08-15 12:14:57 +0200

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './assets/main.css'
 
 import { VueQueryPlugin } from '@tanstack/vue-query'
-import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
@@ -9,7 +8,6 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
 
